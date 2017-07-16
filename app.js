4d9678be636926acc843ceeffffbe084d762a510
odirionye <odirionye@gmail.com>
2017-07-15 23:48:35 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -1,24 +1,69 @@
-var restify = require('restify');
 var builder = require('botbuilder');
+var restify = require('restify');
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
 
-// Create chat connector for communicating with the Bot Framework Service
+// Create chat bot and listen for messages
 var connector = new builder.ChatConnector({
-   // appId: process.env.MICROSOFT_APP_ID,
-    //appPassword: process.env.MICROSOFT_APP_PASSWORD
-appId: "e489dd01-a216-47dc-b475-7f9a36ba35a6",
-appPassword: "2SQUw9tK5f5YFXiQyjt5cNM"
+    appId: "e489dd01-a216-47dc-b475-7f9a36ba35a6",
+    appPassword: "2SQUw9tK5f5YFXiQyjt5cNM"
 });
-
-// Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var userStore = [];
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+    // store user's address
+    var address = session.message.address;
+    userStore.push(address);
+
+    // end current dialog
+    session.endDialog('You\'ve been invited to a survey! It will start in a few seconds...');
 });
+
+// Every 5 seconds, check for new registered users and start a new dialog
+setInterval(function () {
+    var newAddresses = userStore.splice(0);
+    newAddresses.forEach(function (address) {
+
+        console.log('Starting survey for address:', address);
+
+        // new conversation address, copy without conversationId
+        var newConversationAddress = Object.assign({}, address);
+        delete newConversationAddress.conversation;
+
+        // start survey dialog
+        bot.beginDialog(newConversationAddress, 'survey', null, function (err) {
+            if (err) {
+                // error ocurred while starting new conversation. Channel not supported?
+                bot.send(new builder.Message()
+                    .text('This channel does not support this operation: ' + err.message)
+                    .address(address));
+            }
+        });
+
+    });
+}, 5000);
+
+bot.dialog('survey', [
+    function (session) {
+        builder.Prompts.text(session, 'Hello... What\'s your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.number(session, 'Hi ' + results.response + ', How many years have you been coding?');
+    },
+    function (session, results) {
+        session.userData.coding = results.response;
+        builder.Prompts.choice(session, 'What language do you code Node using? ', ['JavaScript', 'CoffeeScript', 'TypeScript']);
+    },
+    function (session, results) {
+        session.userData.language = results.response.entity;
+        session.endDialog('Got it... ' + session.userData.name +
+            ' you\'ve been programming for ' + session.userData.coding +
+            ' years and use ' + session.userData.language + '.');
+    }
+]);
